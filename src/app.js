22b4bb1e6f01267ce4f17962b7d30ed82d4065f5
Chen <Chenpeleg>
2022-02-22 10:31:27 +0200

    added middle ware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import { Form } from './from.mjs'
 import http from 'http';
 
-
+var chunknumber = 0
 
 const server = http.createServer((request, response) => {
+    const data = [];
+
+    let body;
+    request.on('data', (chunk) => {
+        chunknumber = chunknumber + 1;
+        console.log('chunk ' + chunknumber, chunk);
+        data.push(chunk)
+
+    })
+    request.on('end', () => {
+        console.log('end request')
+        body = Buffer.concat(data).toString();
+
+    })
+
     response.setHeader('Content-Type', 'text/html');
     let f = new Form();
     let text = f.getHtml
-    console.log(text);
+
     response.write(text);
     response.end();
 })
--- src/from.mjs
+++ src/from.mjs
@@ -1,8 +1,25 @@
 class Form {
+    #head = `<head>
+    <style>
+body {
+    background-color : pink;
+    font-size : 30px;
 
-    #html = `<form method="POST" action "/"><input name="Say somthing" type="text"><button type="submit"> Press Me!</button> </form>`;
+}
+form {
+    font-size : 30px;
+    margin : 3em;
+}
+button {
+    background-color : gold;
+    border-radius: 10px;
+    padding : 1em;
+    font-size: 30px;
+}
+    </style></head>`
+    #html = `<body><form method="POST" action "/"><br><input name="Say somthing" type="text"><br><input name="uploadfile" type="file"><br> <button type="submit"> Press Me!</button> </form></body>`;
     get getHtml() {
-        return this.#html
+        return this.#head + this.#html
     };
 
 }
